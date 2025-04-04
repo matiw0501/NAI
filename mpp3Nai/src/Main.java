9a02db7033a9f4138faa4494158da8d9f364f128
Mateusz W <s31224@pjwstk.edu.pl>
2025-04-04 20:14:53 +0200

    added languages and fight how to insert text

--- mpp3Nai/src/Main.java
+++ mpp3Nai/src/Main.java
@@ -1,5 +1,30 @@
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ForInput.getOneLevelOfPerceptrons("languages");
+
+        String path = "languages/Polish/Warszawa.txt";
+        List<String> a = new ArrayList<String>();
+        try {
+        a = Files.readAllLines(Paths.get(path));
+            System.out.println(a);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        a.forEach(e -> {
+            System.out.println(Normalizer.normalize(e, Normalizer.Form.NFKD).replaceAll("[^\\p{ASCII}]", ""));
+            // cos z replacem mozna uzyc biblioteki zewnetrznej itd. ale chyba zrob na sztywno zamiane znakow ktorych to nie obsługuje ł ss niemieckie itd.
+        });
+
     }
+
 }
